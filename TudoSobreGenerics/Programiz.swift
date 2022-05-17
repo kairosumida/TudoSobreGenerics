0166fbaff9b0a3736cab97204f680653190fa3fb
kairosumida <kairosumida@hotmail.com>
2022-05-17 17:05:47 -0300

    Finalizado Programiz

--- TudoSobreGenerics/Programiz.swift
+++ TudoSobreGenerics/Programiz.swift
@@ -11,20 +11,37 @@ struct Programiz: View {
     var body: some View {
         ZStack{
             VStack{
+                Text("Função genérica")
                 Text(displayData(data: /*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/))
                 Text(String(displayData(data: 5)))
-                
+                Spacer()
+                Text("Classe genérica")
+                Text(String(Information<Int>(data: 6).getData()))
+                Text(Information<String>(data: "Swift").getData())
+                Spacer()
+                Text("Tipo restritos com genéricos")
+                Text(String(addition(num1: 10, num2: 10)))
             }
         }
-        
     }
     
     func displayData<T>(data: T) -> T{
         return data
     }
-    
 }
-
+func addition<T: Numeric>(num1: T, num2: T) -> T{
+    return num1 + num2
+}
+class Information<T>{
+    
+    var data: T
+    init (data: T){
+        self.data = data
+    }
+    func getData() -> T{
+        return self.data
+    }
+}
 struct Programiz_Previews: PreviewProvider {
     static var previews: some View {
         Programiz()
