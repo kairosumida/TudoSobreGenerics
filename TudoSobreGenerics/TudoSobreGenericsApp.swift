88f8872cccb72b29ce6d6135bb11cb53bf71feee
kairosumida <kairosumida@hotmail.com>
2022-05-17 13:14:48 -0300

    Initial Commit

---
+++ TudoSobreGenerics/TudoSobreGenericsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TudoSobreGenericsApp.swift
+//  TudoSobreGenerics
+//
+//  Created by Kairo Sumida on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TudoSobreGenericsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
